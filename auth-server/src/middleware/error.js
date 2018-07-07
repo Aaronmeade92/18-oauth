1edf3ef7fadc6c34bd3f5d5fc0aed00c047f441e
Aaron <aaronmeade9292@gmail.com>
2018-07-07 11:47:25 -0700

    added most of the scaffold

---
+++ auth-server/src/middleware/error.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ */
+export default  (err,req,res,next) => {
+  let error = {
+    error:(typeof err==='object' && err.message) || err,
+  };
+  res.statusCode = (typeof err==='object' && err.status) || 500;
+  res.statusMessage = (typeof err==='object' && err.statusMessage) || 'Server Error';
+  res.setHeader('Content-Type', 'application/json');
+  res.write( JSON.stringify(error) );
+  res.end();
+};

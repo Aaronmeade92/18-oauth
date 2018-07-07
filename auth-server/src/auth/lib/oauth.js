b4301e5ec2b5a976bcd4dd9d0394cc89dbb29bea
Aaron <aaronmeade9292@gmail.com>
2018-07-07 14:46:49 -0700

    added part of the .env

--- auth-server/src/auth/lib/oauth.js
+++ auth-server/src/auth/lib/oauth.js
@@ -4,33 +4,25 @@ import superagent from 'superagent';
 
 import User from '../model';
 
-// This is currently setup for Google, but we could easily swap it out
-// for any other provider or even use a totally different module to
-// to do this work.
-//
-// So long as the method is called "authorize" and we get the request,
-// we should be able to roll on our own here...
-
 const authorize = (req) => {
 
   let code = req.query.code;
 
   console.log('(1) code', code);
 
-  // exchange the code or a token
   return superagent.post('https://www.googleapis.com/oauth2/v4/token')
     .type('form')
     .send({
       code: code,
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      client_id: process.env.LINKEDIN_CLIENT_ID,
+      client_secret: process.env.LINKEDIN_CLIENT_SECRET,
       redirect_uri: `${process.env.API_URL}/oauth`,
       grant_type: 'authorization_code',
     })
     .then( response => {
-      let googleToken = response.body.access_token;
-      console.log('(2) google token', googleToken);
-      return googleToken;
+      let linkedToken = response.body.access_token;
+      console.log('(2) linkedin token', linkedinToken);
+      return linkedinToken;
     })
   // use the token to get a user
     .then ( token => {
@@ -38,13 +30,13 @@ const authorize = (req) => {
         .set('Authorization', `Bearer ${token}`)
         .then (response => {
           let user = response.body;
-          console.log('(3) Google User', user);
+          console.log('(3) linkedIn User', user);
           return user;
         });
     })
-    .then(googleUser => {
+    .then(linkedinUser => {
       console.log('(4) Creating Account')
-      return User.createFromOAuth(googleUser);
+      return User.createFromOAuth(linkedinUser);
     })
     .then (user => {
       console.log('(5) Created User, generating token');
